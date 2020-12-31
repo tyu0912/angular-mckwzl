dd079dad05a532bb314f2ed2a84aeaa5f414c0c9
tyu0912 <tennisonyu@gmail.com>
2020-12-30 16:55:50 -0800

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartService } from './cart.service';
 
 @NgModule({
   imports: [
@@ -27,7 +28,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductDetailsComponent
   ],
 
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+
+  providers: [CartService]
 
 })
 export class AppModule { }
